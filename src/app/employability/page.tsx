b9c4d0aec1bcb408bd577291f1cf7415b6cf2005
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-10-22 11:13:53 +0530

    Update page.tsx

--- src/app/employability/page.tsx
+++ src/app/employability/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Banner from '@/components/EmployabilitySection/Banner'
+import AboutSection from '@/components/EmployabilitySection/AboutSection'
+import OurAproach from '@/components/EmployabilitySection/OurAproach'
+import WhyRsa from '@/components/EmployabilitySection/WhyRsa'
+import WhatweOffer from '@/components/EmployabilitySection/WhatweOffer'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <Banner />
+    <AboutSection />
+    <OurAproach />
+   <WhatweOffer />
+    <WhyRsa />
+    </>
   )
 }
 
